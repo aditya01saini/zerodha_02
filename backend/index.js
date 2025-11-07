dca1eafeb4a3f78d912cd634895e709e73c1f2f0
aditya01saini <adityasaini35947@gmail.com>
2025-11-07 14:12:36 +0530

    Remove specific CORS origin from middleware
    Removed specific CORS origin configuration.

--- backend/index.js
+++ backend/index.js
@@ -19,10 +19,7 @@ const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 
-app.use(cors({ origin: "https://zerodha-02-dashboard.onrender.com
 
-
-" }));
 
 
 // app.get("/addHoldings", async (req, res) => {
